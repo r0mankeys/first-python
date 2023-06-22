585608891c1c81d0c354ca314bfd44ccc7c88862
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2023-06-22 22:48:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Calculator
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+My first simple python project, I am currently learning python and wanted to do a simple project, and what is more simple (while still being useful) than a calculator
---
+++ calculator.py
@@ -0,0 +1,55 @@
+# Make functions for basic arithmetic operations
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+# Print list of operations for user
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Set a flag to change dpenedent on state of script
+active = True
+
+while active:
+    choice = input("Enter choice (1/2/3/4): ")
+    if choice in ("1", "2", "3", "4"):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            active = False
+    else:
+        print("\nInvalid input please try again\n")
